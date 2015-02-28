eefa526dcf232e272ac26267518a3e540e9cff26
DavidWilson12 <david.b.wilson@comcast.net>
2015-02-27 22:03:08 -0500

    Create SampleCode.lua

---
+++ SampleCode.lua
@@ -0,0 +1,24 @@
+local lq = require(script.Parent.LQueryModule)
+
+local array = {1, 5, 9, 20, 30}
+
+lq(array).filter(function(self)
+	return self > 5
+end)
+
+print(lq(array).concat(", ")) --> 9, 20, 30
+
+lq(array).pushKey("exampleOne", 10, "exampleTwo", 20)
+
+print(array.exampleOne, array.exampleTwo) --> 10 20
+
+lq(array).pushMetatable {
+	__newindex = function(self, key, value)
+		print(tostring(self) .. " [" .. key .. "] = " .. value)
+		rawset(self, key, value)
+	end
+}
+
+lq(array).pushKey("exampleThree", 30) --> table: 0x20828f00 [exampleThree] = 30
+
+print(array.exampleThree) --> 30

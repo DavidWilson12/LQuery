c9456fe6d7f4a1c8faaf34d265f8c45fa937a913
DavidWilson12 <david.b.wilson@comcast.net>
2015-02-27 22:06:06 -0500

    Update SampleCode.lua

--- SampleCode.lua
+++ SampleCode.lua
@@ -2,8 +2,9 @@ local lq = require(script.Parent.LQueryModule)
 
 local array = {1, 5, 9, 20, 30}
 
-lq(array).filter(function(self)
-	return self > 5
+--> returning true keeps 'this' in the array; vice-versa for false
+lq(array).filter(function(this)
+	return this > 5
 end)
 
 print(lq(array).concat(", ")) --> 9, 20, 30
@@ -22,3 +23,7 @@ lq(array).pushMetatable {
 lq(array).pushKey("exampleThree", 30) --> table: 0x20828f00 [exampleThree] = 30
 
 print(array.exampleThree) --> 30
+
+print(lq{}.push(100, 500, 1900).filter(function(this)
+	return this > 100
+end).pop().concat(", ")) --> 500

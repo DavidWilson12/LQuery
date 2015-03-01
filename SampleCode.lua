b767fde7f728a62a098fd453dce0f0858d96585a
DavidWilson12 <david.b.wilson@comcast.net>
2015-03-01 16:49:31 -0500

    Update SampleCode.lua

--- SampleCode.lua
+++ SampleCode.lua
@@ -33,3 +33,18 @@ removeByElement(array, 500)
 --WITH LQUERY:
 local array = {20, 15, 500, "hello"}
 lq(array).popByValue(500)
+
+
+
+--the following is an example of the "HTML" parsing that I'm working on.  It works well, however it doesn't yet support frames inside of frames.
+--note that this is only compatible with the ROBLOX engine
+
+local lq = require(script.Parent:WaitForChild("LQueryModule"))
+local screen = script.Parent:WaitForChild("ScreenGui"):WaitForChild("Screen")
+
+--> add 'screen' to the database of parents that the parser can access
+lq.pushParent("screen", screen)
+
+lq.parseHTML [[
+	<Frame Size=Vector2.new(100, 100); Position=Vector2.new(100, 100); Rotation=45; Parent=screen;> </Frame>
+]]

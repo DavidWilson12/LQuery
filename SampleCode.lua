ff486eace39ae9f39b5208395e5f15fcb9b0ba57
DavidWilson12 <david.b.wilson@comcast.net>
2015-03-01 15:56:13 -0500

    Update SampleCode.lua

--- SampleCode.lua
+++ SampleCode.lua
@@ -1,29 +1,35 @@
 local lq = require("LQuery")
 
-local array = {1, 5, 9, 20, 30}
+--WITHOUT LQUERY:
+local array = {}
+local metatable = {}
+metatable.__newindex = function(self, key, value)
+	print("value '" .. tostring(value) .. "' now stored under key '" .. key .. "')
+end
+setmetatable(array, metatable)
 
---> returning true keeps 'this' in the array; vice-versa for false
-lq(array).filter(function(this)
-	return this > 5
-end)
-
-print(lq(array).concat(", ")) --> 9, 20, 30
-
-lq(array).pushKey("exampleOne", 10, "exampleTwo", 20)
-
-print(array.exampleOne, array.exampleTwo) --> 10 20
-
+--WITH LQUERY
+local array = {}
 lq(array).pushMetatable {
 	__newindex = function(self, key, value)
-		print(tostring(self) .. " [" .. key .. "] = " .. value)
-		rawset(self, key, value)
+		print("value '" .. tostring(value) .. "' now stored under key '" .. key .. "')
 	end
 }
 
-lq(array).pushKey("exampleThree", 30) --> table: 0x20828f00 [exampleThree] = 30
+--WITHOUT LQUERY:
+local array = {20, 15, 500, "hello"}
 
-print(array.exampleThree) --> 30
+function removeByElement(array, element)
+	for i, v in ipairs(array) do
+		if v == element then
+			table.remove(array, i)
+			return
+		end
+	end
+end
 
-print(lq{}.push(100, 500, 1900).filter(function(this)
-	return this > 100
-end).pop().concat(", ")) --> 500
+removeByElement(array, 500)
+
+--WITH LQUERY:
+local array = {20, 15, 500, "hello"}
+lq(array).popByValue(500)
